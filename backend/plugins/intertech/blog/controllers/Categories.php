5225de5d7d1c6018a2ace6b31c38b0e4268256b2
Alexandr Lysak <lysakalexandr42@gmail.com>
2019-09-11 10:44:56 +0300

    Create filters

--- backend/plugins/intertech/blog/controllers/Categories.php
+++ backend/plugins/intertech/blog/controllers/Categories.php
@@ -8,12 +8,14 @@ class Categories extends Controller
     public $implement = [
         'Backend\Behaviors\ListController',
         'Backend\Behaviors\FormController',
-        'Backend\Behaviors\ReorderController'
+        'Backend\Behaviors\ReorderController',
+        'Backend\Behaviors\RelationController'
     ];
     
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
+    public $relationConfig = 'config_relations.yaml';
 
     public function __construct()
     {
